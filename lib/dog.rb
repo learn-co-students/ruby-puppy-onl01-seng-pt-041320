8991f384aaa6cb729a1221f327b3b3eb4393e11d
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-05-01 17:45:09 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+require 'pry'
+
+class Dog
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name) 
+        @name = name
+        #@@all << self
+        self.save
+    end
+
+    def self.all
+        return @@all
+    end
+
+    def self.print_all
+        @@all.each do |names|
+            puts names.name
+        end
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def save
+        @@all.push(self)
+    end
+
+end
