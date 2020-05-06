1def2f8e9b99d5e0c80e7ca181e2ce172f1212da
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-05-06 21:29:13 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,6 +5,7 @@ class Dog
 
 	def initialize(name)
 		@name = name
+<<<<<<< HEAD
 		self.save
 	end
 	
@@ -28,3 +29,26 @@ end
 end 
 
  
+=======
+		@@all << self
+		self << @@all
+	end
+
+	def self.clear_all
+		@@all.clear
+
+	end
+	
+def self.print_all
+  self.all.each|dog| 
+ end
+
+
+	def self.all
+		@@all.each do |dog|
+			puts dog.name
+		 end
+	end 
+ 
+
+>>>>>>> fd14187450fef08232e673b4f31709f171b2da32
