18095aef1e33e1f9f9ab6df98fab93e904fafc78
Ashlen Mencia <ashmencia0103@gmail.com>
2020-05-08 18:09:41 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
 # Add your code here
+class Dog
+  attr_accessor :name, :sleeping
+  @@all = []
+  def initialize(name)
+    @name = name
+    @sleeping = false
+    @@all = self.save
+  end
+  def bark
+    puts "bark bark I'm #{self.name}}"
+  end
+  def self.all
+    @@all
+  end
+  def self.print_all
+    #iterate over @@all array
+    # for each dog object, puts name
+    self.all.each {|dog| puts dog.name}
+  end
+  def self.clear_all
+    self.all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+end
+fido = Dog.new("Fido")
+rover = Dog.new("Rover")
+Dog.all
